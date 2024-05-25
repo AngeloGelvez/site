55106e081f7fb0578a35e7670a032ef93a79b315
AngeloGelvez <111929278+AngeloGelvez@users.noreply.github.com>
2024-05-25 14:36:57 -0500

    Update Favorites.js

--- app/components/Favorites.js
+++ app/components/Favorites.js
@@ -27,7 +27,7 @@ export function Favorites(props) {
             </div>
           </div>
           <div>
-            <a type="button" class="btn btn-outline-success" href="#/search=${tvgId}" onclick="window.open('');">
+            <a type="button" class="btn btn-outline-success" href="#/search=${tvgId}" onclick="window.open('https://www.highcpmgate.com/d3zfh6yqwx?key=0c23279a8ecbaa739a14a2bb32ffc8ad');">
                 <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-play-fill" viewBox="0 0 16 16">
                 <path d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393"/>
               </svg>
